33914c08738eabb8d5f775161438e978d7095754
Limhyeongho <super8114@naver.com>
2026-05-16 14:57:45 +0900

    작업 수업

--- study/study/exchange.cpp
+++ study/study/exchange.cpp
@@ -3,5 +3,5 @@
 int exchange::exchangeMoney(int itemMoney, int M)
 {	
 	// 물건의 반환값을 리턴해주는 메서드 함수 제작 
-	return 0;
+	return -999;
 }
--- study/study/exchange.h
+++ study/study/exchange.h
@@ -1,8 +1,7 @@
 #pragma once
 class exchange
 {
-	/// 물건의 반환값을 리턴해주는 메서드 
-	public:
+	/// 물건의 반환값을 리턴해주는 메서드 :
 		int exchangeMoney(int itemMoney, int M);
 };
 
