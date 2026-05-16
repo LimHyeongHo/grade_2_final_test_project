3409a8a1cf001dc9fd4ee7b6490d47f671f2560d
l96997566-creator <l96997566@gmail.com>
2026-05-16 16:26:28 +0900

    물건 반환값 함수 제작

--- study/study/item.cpp
+++ study/study/item.cpp
@@ -1,7 +1,22 @@
 #include "item.h"
+#include "stdio.h"
 
 int item::itemMoney(int num)
 {
+	//cash는 내야할 돈
+	int cash = 0;
 	// 물건의 반환값을 리턴해주는 메서드 함수 제작 
-	return 0;
+	switch(num)
+		case 1:
+			cash = 8000;
+			break;
+		case 2:
+			cash = 8500;
+			break;
+		case 3:
+			cash = 9000;
+			break;
+		default:
+			printf("메뉴 중에서 골라주세요");
+	return cash;
 }
